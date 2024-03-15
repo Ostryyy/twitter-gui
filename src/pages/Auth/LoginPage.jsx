81218af115d6eb9340014034f8fad3fde849d8b3
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-03-15 20:53:29 +0100

    Initial commit with created view of login, register pages

---
+++ src/pages/Auth/LoginPage.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@mui/material";
+import classes from "./Auth.module.css";
+import logo from "../../assets/images/logo.png";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={classes.container}>
+      <Paper className={classes.box} elevation={2}>
+        <div className={classes.logoContainer}>
+          <img src={logo} alt="Logo" />
+        </div>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Address Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+        </form>
+      </Paper>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/Auth/RegisterPage.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { TextField, Button, Paper, Typography } from "@mui/material";
+import classes from "./Auth.module.css";
+import logo from "../../assets/images/logo.png";
+
+function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={classes.container}>
+      <Paper className={classes.box} elevation={2}>
+        <div className={classes.logoContainer}>
+          <img src={logo} alt="Logo" />
+        </div>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Address Email"
+            name="email"
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="new-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Register
+          </Button>
+        </form>
+      </Paper>
+    </div>
+  );
+}
+
+export default RegisterPage;
